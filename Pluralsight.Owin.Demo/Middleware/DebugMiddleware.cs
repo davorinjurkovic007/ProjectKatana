58fd3b44b657edff122c9f20311e982950cf4a8f
Breskva <davorin.jurkovic@gmail.com>
2020-02-19 22:05:59 +0100

    Building a Simple OWIN Pipeline and Creating Middleware with OWIN

---
+++ Pluralsight.Owin.Demo/Middleware/DebugMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using AppFunc = System.Func<
+    System.Collections.Generic.IDictionary<string, object>,
+    System.Threading.Tasks.Task
+>;
+
+namespace Pluralsight.Owin.Demo
+{
+    public class DebugMiddleware
+    {
+        readonly AppFunc _next;
+        private readonly DebugMiddlewareOptions options;
+
+        public DebugMiddleware(AppFunc next, DebugMiddlewareOptions options)
+        {
+            _next = next;
+            this.options = options;
+
+            if(this.options.OnIncomingRequest == null)
+            {
+                this.options.OnIncomingRequest = ctx => { Debug.WriteLine("Incoming request in Options: " + ctx.Request.Path); };
+            }
+
+            if (this.options.OnOutgoingRequest == null)
+            {
+                this.options.OnOutgoingRequest = ctx => { Debug.WriteLine("Outgoing request in Options: " + ctx.Request.Path); };
+            }
+        }
+
+        public async Task Invoke(IDictionary<string, object> enviroment)
+        {
+            var ctx = new OwinContext(enviroment);
+            // Alternative call
+            // var path = (string)enviroment["owin.RequestPath"];
+
+            options.OnIncomingRequest(ctx);
+            await _next(enviroment);
+            options.OnOutgoingRequest(ctx);
+        }
+    }
+}
---
+++ Pluralsight.Owin.Demo/Middleware/DebugMiddlewareExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Owin;
+using Pluralsight.Owin.Demo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Owin
+{
+    public static class DebugMiddlewareExtensions
+    {
+        public static void UseDebugMiddleware(this IAppBuilder app, DebugMiddlewareOptions options = null)
+        {
+            if(options == null)
+            {
+                options = new DebugMiddlewareOptions();
+            }
+
+            app.Use<DebugMiddleware>(options);
+        }
+    }
+}
